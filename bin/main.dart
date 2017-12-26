614ed52161ce70cb1b8560b173b3484d7eafa507
Ravi Teja Gudapati <tejainece@gmail.com>
2017-12-26 11:04:01 +0100

    Serve static files

---
+++ bin/main.dart
@@ -0,0 +1,19 @@
+import 'package:jaguar/jaguar.dart';
+import 'package:path/path.dart' as p;
+
+main(List<String> arguments) async {
+  final server = new Jaguar();
+
+  // Serve with prefix stripped. Similar to 'alias' in Nginx.
+  server.staticFiles('/static/*', 'static');
+
+  // Serve with prefix intact. Similar to 'root' in Nginx.
+  server.staticFiles('/img/*', 'static', stripPrefix: false);
+
+  // Example with no correlation between request uri and target path
+  server.staticFiles('/media/img/*', p.join('static', 'img'));
+
+  // Example that serves only one file
+  server.staticFile('/hello', p.join('static', 'hello.txt'));
+  await server.serve();
+}
